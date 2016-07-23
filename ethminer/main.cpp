040d9d3c433619c665384518907bfd1cc8605450
Bob Summerwill <bob@summerwill.net>
2016-07-22 18:51:32 -0700

    Looks like we do need that ugly chunk of stuff for WinSocks in ethminer.

--- ethminer/main.cpp
+++ ethminer/main.cpp
@@ -20,6 +20,18 @@
  * Ethereum client.
  */
 
+
+
+// Solves the problem of including windows.h before including winsock.h		
+// as detailed here:		
+// http://stackoverflow.com/questions/1372480/c-redefinition-header-files-winsock2-h		
+
+#if defined(_WIN32)	
+	#define _WINSOCKAPI_
+	#include <windows.h>
+#endif // defined(_WIN32)
+
+
 #include "MinerAux.h"
 
 using namespace std;
