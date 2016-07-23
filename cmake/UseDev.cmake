8b0ea6437a89c20350577436a31c30ca54540387
Bob Summerwill <bob@summerwill.net>
2016-07-23 06:30:21 -0700

    Stripped out the last bits of "base" support. Updated the diagram. All the pthreads dependencies are now done explicitly.

--- cmake/UseDev.cmake
+++ cmake/UseDev.cmake
@@ -1,9 +1,3 @@
 function(eth_apply TARGET REQUIRED SUBMODULE)
 
-	if (${SUBMODULE} STREQUAL "base")
-		if (UNIX AND NOT APPLE)
-			target_link_libraries(${TARGET} pthread)
-		endif()
-	endif()
-
 endfunction()
